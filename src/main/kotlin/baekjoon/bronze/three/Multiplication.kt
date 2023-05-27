26c62f22721dfd4d8c37c877cf5121390ea0021c
yongrokkwon <yongrok.kwon@gmail.com>
2023-05-27 23:00:45 +0900

    Add Multiplication

--- src/main/kotlin/baekjoon/bronze/three/Multiplication.kt
+++ src/main/kotlin/baekjoon/bronze/three/Multiplication.kt
@@ -1,5 +1,19 @@
 package baekjoon.bronze.three
 
 fun main() {
+    val first = readln().toInt()
+    val second = readln()
 
+    val result = arrayListOf<Int>()
+
+    second.forEachIndexed { index, c ->
+        result.add(c.digitToInt() * first)
+    }
+    result.reverse()
+
+    result.forEach {
+        println(it)
+    }
+
+    println(first * second.toInt())
 }
